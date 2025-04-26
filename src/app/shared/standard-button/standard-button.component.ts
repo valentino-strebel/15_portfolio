94e4fd01ab790bdfa3f1f5e12615bc5de92ee5b1
Valentino Strebel <tino_schweizer@hotmail.com>
2025-04-26 15:03:27 +0200

    update

---
+++ src/app/shared/standard-button/standard-button.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-standard-button',
+  standalone: true,
+  imports: [],
+  templateUrl: './standard-button.component.html',
+  styleUrl: './standard-button.component.scss'
+})
+export class StandardButtonComponent {
+
+}
