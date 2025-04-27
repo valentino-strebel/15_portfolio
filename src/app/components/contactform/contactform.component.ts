5bba8a92b34424bb82c5d6832a5a90c8ac59c8c9
Valentino Strebel <tino_schweizer@hotmail.com>
2025-04-27 17:23:09 +0200

    update

--- src/app/components/contactform/contactform.component.ts
+++ src/app/components/contactform/contactform.component.ts
@@ -1,6 +1,7 @@
-import { Component, NgModule } from '@angular/core';
+import { Component, NgModule, inject } from '@angular/core';
 import { FormsModule, NgForm } from '@angular/forms';
 import { SubmitButtonComponent } from '../../shared/submit-button/submit-button.component';
+import { HttpClient } from '@angular/common/http';
 
 @Component({
   selector: 'app-contactform',
@@ -10,15 +11,42 @@ import { SubmitButtonComponent } from '../../shared/submit-button/submit-button.
   styleUrl: './contactform.component.scss',
 })
 export class ContactformComponent {
+  http = inject(HttpClient);
+
   contactData = {
     name: '',
     email: '',
     message: '',
   };
 
+  mailTest = true;
+
+  post = {
+    endPoint: 'https://strebel-company.de/sendMail.php',
+    body: (payload: any) => JSON.stringify(payload),
+    options: {
+      headers: {
+        'Content-Type': 'text/plain',
+        responseType: 'text',
+      },
+    },
+  };
+
   onSubmit(ngForm: NgForm) {
-    if (ngForm.valid && ngForm.submitted) {
-      console.log(this.contactData);
+    if (ngForm.submitted && ngForm.form.valid && !this.mailTest) {
+      this.http
+        .post(this.post.endPoint, this.post.body(this.contactData))
+        .subscribe({
+          next: (response) => {
+            ngForm.resetForm();
+          },
+          error: (error) => {
+            console.error(error);
+          },
+          complete: () => console.info('send post complete'),
+        });
+    } else if (ngForm.submitted && ngForm.form.valid && this.mailTest) {
+      ngForm.resetForm();
     }
   }
 }
