2257aadc45ada1d8ff35b5cb5c54b53c53120870
Valentino Strebel <tino_schweizer@hotmail.com>
2025-05-06 13:16:55 +0200

    update

--- src/app/components/skills/skill-logos/skill-logos.component.ts
+++ src/app/components/skills/skill-logos/skill-logos.component.ts
@@ -9,17 +9,50 @@ import { Component } from '@angular/core';
   styleUrl: './skill-logos.component.scss',
 })
 export class SkillLogosComponent {
-  images: string[] = [
-    '../../assets/img/icons/icon_text/HTML_icon.svg',
-    '../../assets/img/icons/icon_text/CSS_icon.svg',
-    '../../assets/img/icons/icon_text/JS_icon.svg',
-    '../../assets/img/icons/icon_text/TS_icon.svg',
-    '../../assets/img/icons/icon_text/ANG_icon.svg',
-    '../../assets/img/icons/icon_text/FIRE_icon.svg',
-    '../../assets/img/icons/icon_text/API_icon.svg',
-    '../../assets/img/icons/icon_text/GIT_icon.svg',
-    '../../assets/img/icons/icon_text/mat_des_icon.svg',
-    '../../assets/img/icons/icon_text/SCRUM_icon.svg',
-    '../../assets/img/icons/icon_text/cont_learn_icon.svg',
+  skills = [
+    {
+      image: '../../assets/img/icons/icon_plain/front/html_plain.svg',
+      text: 'HTML',
+    },
+    {
+      image: '../../assets/img/icons/icon_plain/front/css_plain.svg',
+      text: 'CSS',
+    },
+    {
+      image: '../../assets/img/icons/icon_plain/front/js_plain.svg',
+      text: 'JavaScript',
+    },
+    {
+      image: '../../assets/img/icons/icon_plain/front/ts_plain.svg',
+      text: 'TypeScript',
+    },
+    {
+      image: '../../assets/img/icons/icon_plain/front/ang_plain.svg',
+      text: 'Angular',
+    },
+    {
+      image: '../../assets/img/icons/icon_plain/front/fire_plain.svg',
+      text: 'Firebase',
+    },
+    {
+      image: '../../assets/img/icons/icon_plain/front/api_plain.svg',
+      text: 'Rest Api',
+    },
+    {
+      image: '../../assets/img/icons/icon_plain/front/git_plain.svg',
+      text: 'Git',
+    },
+    {
+      image: '../../assets/img/icons/icon_plain/front/mat_des_plain.svg',
+      text: 'Material Design',
+    },
+    {
+      image: '../../assets/img/icons/icon_plain/front/scrum_plain.svg',
+      text: 'Scrum',
+    },
+    {
+      image: '../../assets/img/icons/icon_plain/front/cont_learn_plain.svg',
+      text: 'Continually learning',
+    },
   ];
 }
