c49f8077ddcb864447222254888a2e9ef043d33e
Valentino Strebel <tino_schweizer@hotmail.com>
2025-09-19 11:12:36 +0200

    update

--- src/app/components/reference/reference.component.ts
+++ src/app/components/reference/reference.component.ts
@@ -35,8 +35,8 @@ export class ReferenceComponent implements AfterViewInit {
         de: 'SpryFlash GmbH - Salzburg, Österreich',
       },
       text: {
-        en: 'Mr. Strebel impressed with his efficient, self-reliant work style and strong professional expertise. In his role as Project Manager, he successfully led complex IT projects and applied his technical knowledge to great effect.',
-        de: 'Herr Strebel überzeugte durch seine effiziente, eigenverantwortliche Arbeitsweise und hohe Fachkompetenz. In seiner Rolle als Projektmanager steuerte er komplexe IT-Projekte souverän und brachte sein technisches Know-how gewinnbringend ein.',
+        en: '"Mr. Strebel impressed with his efficient, self-reliant work style and strong professional expertise. In his role as Project Manager, he successfully led complex IT projects and applied his technical knowledge to great effect."',
+        de: '"Herr Strebel überzeugte durch seine effiziente, eigenverantwortliche Arbeitsweise und hohe Fachkompetenz. In seiner Rolle als Projektmanager steuerte er komplexe IT-Projekte souverän und brachte sein technisches Know-how gewinnbringend ein."',
       },
     },
     {
@@ -46,8 +46,8 @@ export class ReferenceComponent implements AfterViewInit {
         de: 'Novilio Consulting GmbH - München, Deutschland',
       },
       text: {
-        en: 'Mr. Strebel impressed with his structured, detail-oriented approach and solid methodological expertise in requirements engineering. In client projects, he took responsibility for quality assurance, process consulting, and agile coordination.',
-        de: 'Herr Strebel überzeugte durch seine strukturierte, sorgfältige Arbeitsweise und sein sicheres methodisches Vorgehen im Requirements Engineering. In Kundenprojekten übernahm er Verantwortung für die Qualitätssicherung, Prozessberatung und agile Koordination.',
+        en: '"Mr. Strebel impressed with his structured, detail-oriented approach and solid methodological expertise in requirements engineering. In client projects, he took responsibility for quality assurance, process consulting, and agile coordination."',
+        de: '"Herr Strebel überzeugte durch seine strukturierte, sorgfältige Arbeitsweise und sein sicheres methodisches Vorgehen im Requirements Engineering. In Kundenprojekten übernahm er Verantwortung für die Qualitätssicherung, Prozessberatung und agile Koordination."',
       },
     },
     {
@@ -57,8 +57,8 @@ export class ReferenceComponent implements AfterViewInit {
         de: 'ALDI Süd Holding - Salzburg, Österreich',
       },
       text: {
-        en: 'Mr. Strebel possesses profound professional expertise in requirements assessment. He is able to maintain an overview even in very stressful situations and to initiate target-oriented measures taking into account all relevant developments.',
-        de: 'Herr Strebel verfügt über fundierte fachliche Expertise in der Anforderungsanalyse. Er ist auch in sehr stressigen Situationen in der Lage, den Überblick zu behalten und unter Berücksichtigung aller relevanten Entwicklungen zielgerichtete Maßnahmen einzuleiten.',
+        en: '"Mr. Strebel possesses profound professional expertise in requirements assessment. He is able to maintain an overview even in very stressful situations and to initiate target-oriented measures taking into account all relevant developments."',
+        de: '"Herr Strebel verfügt über fundierte fachliche Expertise in der Anforderungsanalyse. Er ist auch in sehr stressigen Situationen in der Lage, den Überblick zu behalten und unter Berücksichtigung aller relevanten Entwicklungen zielgerichtete Maßnahmen einzuleiten."',
       },
     },
   ];
